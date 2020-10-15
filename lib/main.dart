aceabbe04f4deeff1cf75c0fa2c4adb759def0f1
Heitor Augusto Melecardi do Amaral <heitor.amaral90@outlook.com>
2020-10-15 14:26:02 -0300

    Scaffolding a Flutter App.

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,26 @@ import 'package:flutter/material.dart';
 
 //The main function is the starting point for all Flutter Apps.
 void main() {
-  runApp(
-    MaterialApp(
-      home: Center(
-        child: Text(
-          'Hello World!',
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg'),
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
